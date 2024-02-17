4975bc6f4dfb36f631aa5b78f117dd5cce0b37bc
Ihsan Tri Marseno <ihsanmarseno09@gmail.com>
2024-02-17 13:00:51 +0700

    Add URL Deploy

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -16,8 +16,8 @@ export async function POST(request) {
     });
 
     const session = await stripe.checkout.sessions.create({
-      success_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cancel",
+      success_url: `https://next-store-zustand.vercel.app/success`,
+      cancel_url: "https://next-store-zustand.vercel.app/cancel",
       line_items: body.lineItems,
       mode: "payment",
     });
